5cd29f184970557507eb5c17d97289fdc17a0caf
Dhananjay Kaushik <dhananjay@centilytics.com>
2021-04-10 19:33:58 +0530

    changes done

--- maze-generator/src/app/app.component.ts
+++ maze-generator/src/app/app.component.ts
@@ -1,10 +1,232 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { Group } from 'three';
+import { Cell } from './classes/Cell';
+import { Player } from './classes/Player';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'maze-generator';
+
+  ngOnInit() {
+    // const OrbitControls = oc(THREE);
+    // Grid constants
+    const GRID_HEIGHT = 10;
+    const GRID_LENGTH = 10;
+
+    // Creating Scene
+    const scene = new THREE.Scene();
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      12,
+      window.innerWidth / window.innerHeight,
+      1,
+      100
+    );
+    scene.add(camera);
+
+    // Creating renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector('.webgl') as HTMLCanvasElement,
+      antialias: true,
+      alpha: true,
+    });
+    // renderer.setClearColor('')
+
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.update();
+    // const axesHelper = new THREE.AxesHelper(12);
+    // scene.add(axesHelper);
+
+    const keyGen = (x: number, y: number, z: number) => {
+      return `${x}|${y}|${z}`;
+    };
+
+    // Creating grid
+    const gridMap: Map<String, Cell> = new Map();
+    for (let i = 0; i < GRID_LENGTH; ++i) {
+      for (let j = 0; j < GRID_HEIGHT; ++j) {
+        const cell = new Cell(i, 1, j);
+        if (i === 0 || i === GRID_LENGTH - 1 || j === 0) {
+          cell.isWall = true;
+        }
+        if (i === 1 && j === 0) {
+          // Starting Point
+          cell.isWall = false;
+          cell.isStartingCell = true;
+        }
+        if (i === GRID_LENGTH - 2 && j === GRID_HEIGHT - 1) {
+          cell.isEndingCell = true;
+        }
+        gridMap.set(keyGen(i, 1, j), cell);
+      }
+    }
+
+    const getValidateNeighbor = (
+      neighbors: { x: number; y: number; z: number }[]
+    ) => {
+      const validNeighbors = [];
+      neighbors.forEach((neighbor) => {
+        const neighborData = gridMap.get(
+          keyGen(neighbor.x, neighbor.y, neighbor.z)
+        );
+        if (neighborData && !neighborData.isVisited && !neighborData.isWall) {
+          validNeighbors.push(neighbor);
+        }
+      });
+      if (validNeighbors && validNeighbors.length) {
+        // if (validNeighbors.length === 1) {
+        //   return validNeighbors[0];
+        // }
+        return validNeighbors[
+          Math.floor(Math.random() * validNeighbors.length)
+        ];
+      }
+      return null;
+    };
+    const removeWall = (current: Cell, next: Cell) => {
+      //       Back  Right Front Left
+      if (current.x - next.x === -1) {
+        // Next is right to Current
+        next.walls[3] = false;
+        current.walls[1] = false;
+      }
+      if (current.x - next.x === 1) {
+        // Next is left to Current
+        next.walls[1] = false;
+        current.walls[3] = false;
+      }
+      if (current.z - next.z === -1) {
+        // Next is front of Current
+        next.walls[0] = false;
+        current.walls[2] = false;
+      }
+      if (current.z - next.z === 1) {
+        // Next is back of Current
+        next.walls[2] = false;
+        current.walls[0] = false;
+      }
+    };
+    // Starting Cell :: 1 1 0
+    let current = gridMap.get(keyGen(1, 1, 0));
+    const cellStack = [];
+    const mazeGenerate = () => {
+      if (current) {
+        current.isVisited = true;
+        const neighbors = current.getNeigbors();
+        const nextCoordinates = getValidateNeighbor(neighbors);
+        if (!neighbors && !cellStack.length) {
+          return;
+        }
+        if (nextCoordinates !== null) {
+          const next = gridMap.get(
+            keyGen(nextCoordinates.x, nextCoordinates.y, nextCoordinates.z)
+          );
+          if (next) {
+            cellStack.push(current);
+            // Remove walls between current and next
+            removeWall(current, next);
+            next.isVisited = true;
+            current = next;
+            mazeGenerate();
+          }
+        } else if (cellStack.length) {
+          current = cellStack.pop();
+          mazeGenerate();
+        }
+        return;
+      }
+    };
+    mazeGenerate();
+
+    const gridGroup = new Group();
+    for (let value of gridMap.values()) {
+      value.drawCube();
+      gridGroup.add(value.group);
+    }
+    // const geometry = new THREE.SphereGeometry(1, 16, 12);
+    // const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    // const sphere = new THREE.Mesh(geometry, material);
+    // sphere.position.set(1.5, 1.5, 0);
+    // sphere.scale.set(0.3, 0.3, 0.3);
+    const player = new Player(1, 1, 0);
+    player.drawCube();
+    camera.position.set(GRID_LENGTH * 1.5, GRID_LENGTH * 5, GRID_LENGTH * 3.5);
+
+    // gridGroup.position.x = -Math.floor(GRID_LENGTH / 2);
+    // gridGroup.position.y = 1;
+    // gridGroup.position.z = -Math.floor(GRID_HEIGHT / 2);
+
+    const mainGroup = new THREE.Group();
+    mainGroup.add(gridGroup, player.group);
+    scene.add(mainGroup);
+
+    window.addEventListener('keydown', (event) => {
+      const key = event.key.toLowerCase();
+      switch (key) {
+        case 'arrowup':
+        case 'w':
+          player.moveBack(camera, gridMap, keyGen, GRID_HEIGHT, GRID_LENGTH);
+          break;
+        case 'arrowright':
+        case 'd':
+          player.moveLeft(camera, gridMap, keyGen, GRID_HEIGHT, GRID_LENGTH);
+          break;
+        case 'arrowdown':
+        case 's':
+          player.moveForward(camera, gridMap, keyGen, GRID_HEIGHT, GRID_LENGTH);
+          break;
+        case 'arrowleft':
+        case 'a':
+          player.moveRight(camera, gridMap, keyGen, GRID_HEIGHT, GRID_LENGTH);
+          break;
+      }
+    });
+
+    let cameraMoved = null;
+
+    const renderScene = () => {
+      requestAnimationFrame(renderScene);
+      if (
+        player.group.position.z > GRID_HEIGHT / 2 &&
+        player.group.position.x > GRID_LENGTH / 2
+      ) {
+        if (!cameraMoved) {
+          cameraMoved = {
+            x: camera.position.x,
+            y: camera.position.y,
+            z: camera.position.z,
+          };
+          gsap.to(camera.position, {
+            x: camera.position.x + 4,
+            y: camera.position.y + 6,
+            z: camera.position.z + 12,
+            duration: 0.5,
+          });
+        }
+      } else {
+        if (cameraMoved) {
+          // Back to originals
+          gsap.to(camera.position, {
+            x: cameraMoved.x,
+            y: cameraMoved.y,
+            z: cameraMoved.z,
+            duration: 0.5,
+          });
+          cameraMoved = null;
+        }
+      }
+      camera.lookAt(mainGroup.position);
+      // controls.update();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+
+    renderScene();
+  }
 }
---
+++ maze-generator/src/app/classes/Cell.ts
@@ -0,0 +1,176 @@
+import * as THREE from 'three';
+import { TextureLoader } from 'three';
+
+export class Cell {
+  readonly CELL_HEIGHT = 1;
+  readonly CELL_WIDTH = 1;
+  readonly CELL_DEPTH = 1;
+  readonly PLAYER_HEIGHT = 0.5;
+  readonly PLAYER_WIDTH = 0.5;
+  readonly PLAYER_DEPTH = 0.5;
+  readonly THICKNESS = 0.02;
+  public group: THREE.Group = new THREE.Group();
+  isVisited = false;
+  isWall = false;
+  isStartingCell = false;
+  isEndingCell = false;
+  turnRed = false;
+  turnGreen = false;
+  //       Back  Right Front Left
+  walls = [true, true, true, true];
+  constructor(
+    public x: number,
+    public y: number,
+    public z: number,
+    public isPlayer?: boolean
+  ) {}
+
+  drawCube() {
+    const material = new THREE.MeshBasicMaterial({ color: 0x0f2027 });
+    const materialFloor = new THREE.MeshBasicMaterial({ color: 0x2c5364 });
+    const materialPlayer = new THREE.MeshBasicMaterial({
+      map: new TextureLoader().load('src/assets/box.png'),
+    });
+
+    const dimensions = {
+      height: this.isPlayer ? this.PLAYER_HEIGHT : this.CELL_HEIGHT,
+      width: this.isPlayer ? this.PLAYER_WIDTH : this.CELL_WIDTH,
+      depth: this.isPlayer ? this.PLAYER_DEPTH : this.CELL_DEPTH,
+    };
+
+    // Front face creation
+    const front = new THREE.Mesh(
+      new THREE.BoxGeometry(
+        dimensions.width,
+        dimensions.height,
+        this.THICKNESS
+      ),
+      this.isPlayer ? materialPlayer : material
+    );
+    front.position.set(
+      this.x + dimensions.width / 2,
+      this.y + dimensions.height / 2,
+      this.z
+    );
+
+    // Back face creation
+    const back = new THREE.Mesh(
+      new THREE.BoxGeometry(
+        dimensions.width,
+        dimensions.height,
+        this.THICKNESS
+      ),
+      this.isPlayer ? materialPlayer : material
+    );
+    back.position.set(
+      this.x + dimensions.width / 2,
+      this.y + dimensions.height / 2,
+      this.z - dimensions.depth
+    );
+
+    // Right face creation
+    const right = new THREE.Mesh(
+      new THREE.BoxGeometry(
+        this.THICKNESS,
+        dimensions.height,
+        dimensions.depth
+      ),
+      this.isPlayer ? materialPlayer : material
+    );
+    right.position.set(
+      this.x + dimensions.width,
+      this.y + dimensions.height / 2,
+      this.z - dimensions.depth / 2
+    );
+
+    // Left face creation
+    const left = new THREE.Mesh(
+      new THREE.BoxGeometry(
+        this.THICKNESS,
+        dimensions.height,
+        dimensions.depth
+      ),
+      this.isPlayer ? materialPlayer : material
+    );
+    left.position.set(
+      this.x,
+      this.y + dimensions.height / 2,
+      this.z - dimensions.depth / 2
+    );
+
+    // Bottom face
+    const bottom = new THREE.Mesh(
+      new THREE.BoxGeometry(dimensions.width, this.THICKNESS, dimensions.depth),
+      this.isPlayer ? materialPlayer : materialFloor
+    );
+    bottom.position.set(
+      this.x + dimensions.width / 2,
+      this.y,
+      this.z - dimensions.depth / 2
+    );
+
+    // Top face
+    const top = new THREE.Mesh(
+      new THREE.BoxGeometry(dimensions.width, this.THICKNESS, dimensions.depth),
+      this.isPlayer ? materialPlayer : material
+    );
+    top.position.set(
+      this.x + dimensions.width / 2,
+      this.y + dimensions.height,
+      this.z - dimensions.depth / 2
+    );
+
+    this.group = new THREE.Group();
+    if (!this.isStartingCell) {
+      if (this.walls[0]) {
+        this.group.add(back);
+      }
+      if (this.walls[2] && !this.isEndingCell) {
+        this.group.add(front);
+      }
+    }
+    if (this.walls[1]) {
+      this.group.add(right);
+    }
+    if (this.walls[3]) {
+      this.group.add(left);
+    }
+    this.group.add(bottom);
+    if (this.isWall || this.isPlayer) {
+      this.group.add(top);
+    }
+    if (this.isPlayer) {
+      this.group.position.set(
+        this.group.position.x + dimensions.width / 2,
+        this.group.position.y + dimensions.height / 2,
+        this.group.position.z - dimensions.depth / 2
+      );
+    }
+  }
+
+  getNeigbors() {
+    // [ BACK, RIGHT, FRONT, LEFT ]
+    return [
+      {
+        x: this.x,
+        y: this.y,
+        z: this.z - 1,
+      },
+      {
+        x: this.x + 1,
+        y: this.y,
+        z: this.z,
+      },
+      {
+        x: this.x,
+        y: this.y,
+        z: this.z + 1,
+      },
+      {
+        x: this.x - 1,
+        y: this.y,
+        z: this.z,
+      },
+    ];
+  }
+}
