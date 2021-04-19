1786a11f41d57ba7782a85116b7084dfe414fdf0
Dhananjay Kaushik <dhananjay@centilytics.com>
2021-04-19 23:37:51 +0530

    changes done

--- light-materials/src/app/app.component.ts
+++ light-materials/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { GUI } from 'dat-gui';
+import { fromEvent } from 'rxjs';
 import {
 	AmbientLight,
+	BufferAttribute,
 	DoubleSide,
 	Mesh,
-	MeshToonMaterial,
+	MeshStandardMaterial,
 	PerspectiveCamera,
 	PlaneBufferGeometry,
 	PointLight,
@@ -27,13 +29,21 @@ export class AppComponent implements OnInit {
 		 * Loading Textures
 		 */
 		const textureLoader = new TextureLoader();
-		const texture = textureLoader.load('../assets/box.png');
+		const boxTexture = textureLoader.load('../assets/box.png');
+		const gradientTexture = textureLoader.load('../assets/3.jpg');
+		const doorTexture = textureLoader.load('../assets/door/door.jpg');
+		const aoTexture = textureLoader.load(
+			'../assets/door/ambientOcclusion.jpg'
+		);
+		const doorHeightTexture = textureLoader.load(
+			'../assets/door/height.jpg'
+		);
 
 		/**
 		 * Materials
 		 */
 		// const material = new MeshBasicMaterial({
-		// 	map: texture,
+		// 	map: boxTexture,
 		// });
 		// const material = new MeshNormalMaterial();
 		// const material = new MeshMatcapMaterial();
@@ -42,14 +52,38 @@ export class AppComponent implements OnInit {
 		// const material = new MeshPhongMaterial();
 		// material.shininess = 100;
 		// material.specular = new Color(0x00ff00);
-		const material = new MeshToonMaterial();
+		// const material = new MeshToonMaterial();
+		// material.gradientMap = gradientTexture;
+		// gradientTexture.minFilter = NearestFilter;
+		// gradientTexture.magFilter = NearestFilter;
+		// gradientTexture.generateMipmaps = false;
+
+		const material = new MeshStandardMaterial();
+		material.metalness = 0.5;
+		material.roughness = 0.5;
+		material.map = doorTexture;
+		material.aoMap = aoTexture;
+		material.aoMapIntensity = 1;
+		material.displacementMap = doorHeightTexture;
+		material.displacementScale = 0.01;
+
+		/**
+		 * Mesh Attributes
+		 */
+		const meshAttributes = {
+			gap: 2.5,
+		};
 
 		/**
 		 * Creating sphere Mesh
 		 */
 		const sphereMesh = new Mesh(
-			new SphereBufferGeometry(1, 20, 20),
+			new SphereBufferGeometry(0.5, 64, 64),
 			material
+		);
+		sphereMesh.geometry.setAttribute(
+			'uv2',
+			new BufferAttribute(sphereMesh.geometry.attributes.uv.array, 2)
 		);
 
 		/**
@@ -57,26 +91,32 @@ export class AppComponent implements OnInit {
 		 */
 		material.side = DoubleSide;
 		const planeMesh = new Mesh(
-			new PlaneBufferGeometry(2, 2, 4, 4),
+			new PlaneBufferGeometry(1, 1, 100, 100),
 			material
 		);
-		planeMesh.position.x = -2.5;
+		planeMesh.geometry.setAttribute(
+			'uv2',
+			new BufferAttribute(planeMesh.geometry.attributes.uv.array, 2)
+		);
 
 		/**
 		 * Creating Torus Mesh
 		 */
 		const torusMesh = new Mesh(
-			new TorusBufferGeometry(0.5, 0.3, 20, 100),
+			new TorusBufferGeometry(0.3, 0.2, 64, 128),
 			material
 		);
-		torusMesh.position.x = 2.5;
+		torusMesh.geometry.setAttribute(
+			'uv2',
+			new BufferAttribute(torusMesh.geometry.attributes.uv.array, 2)
+		);
 
 		/**
 		 * Camera
 		 */
 		const camera = new PerspectiveCamera(75, this.aspect, 0.1, 100);
 		camera.position.z = -4;
-		camera.lookAt(sphereMesh.position);
+		// camera.lookAt(sphereMesh.position);
 
 		/**
 		 * Scene
@@ -111,12 +151,60 @@ export class AppComponent implements OnInit {
 		scene.add(pointLight);
 
 		/**
+		 * Handling Events
+		 */
+		const handleEvents = () => {
+			/**
+			 * Handling Resize
+			 */
+			fromEvent(window, 'resize').subscribe(() => {
+				renderer.setSize(this.width, this.height);
+				scene.updateMatrix();
+			});
+		};
+		handleEvents();
+
+		/**
 		 * Debugger
 		 */
 		const datGui = new GUI();
 		if ('wireframe' in material) {
 			datGui.add(material, 'wireframe').name('Wireframe');
 		}
+		if ('metalness' in material) {
+			datGui
+				.add(material, 'metalness')
+				.min(0)
+				.max(1)
+				.step(0.0001)
+				.name('Metalness');
+		}
+		if ('roughness' in material) {
+			datGui
+				.add(material, 'roughness')
+				.min(0)
+				.max(1)
+				.step(0.0001)
+				.name('Roughness');
+		}
+		if ('aoMapIntensity' in material) {
+			datGui
+				.add(material, 'aoMapIntensity')
+				.min(0)
+				.max(10)
+				.step(1)
+				.name('AO-Map Intensity');
+		}
+		if ('displacementScale' in material) {
+			datGui
+				.add(material, 'displacementScale')
+				.min(0)
+				.max(1)
+				.step(0.001)
+				.name('Displacement Scale');
+		}
+
+		datGui.add(meshAttributes, 'gap').min(0).max(5).step(0.1).name('Gap');
 		datGui.domElement = document.querySelector('body') as HTMLElement;
 
 		/**
@@ -146,6 +234,12 @@ export class AppComponent implements OnInit {
 			torusMesh.rotation.x = elapsedTime * 0.0002;
 			torusMesh.rotation.y = elapsedTime * 0.0002;
 			torusMesh.rotation.z = elapsedTime * 0.0002;
+
+			/**
+			 * Updating Gap
+			 */
+			planeMesh.position.x = -meshAttributes.gap;
+			torusMesh.position.x = meshAttributes.gap;
 
 			/**
 			 * Orbit Controls Update
